8011ca7cd35540f658f431d6547100625bec75c6
webtech-pmp <webtech-pmp@gmail.com>
2019-07-06 21:38:28 +0000

    use Methods instead

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+    logState: function() {
+      console.log(
+        `${this.title} has${this.complete ? " " : " not "}been completed`
+      );
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
 }
 
-// prints out the provided task's details
-function logTaskState(task) {
-  console.log(
-    `${task.title} has${task.complete ? " " : " not "}been completed`
-  );
-}
-
-// marks the provided task as completed
-function completeTask(task) {
-  task.complete = true;
-}
-
-// DRIVER CODE BELOW
+// DRIVER CODE CHANGES BELOW
 
 const task1 = newTask(
   "Clean Cat Litter",
@@ -28,8 +26,6 @@ const task1 = newTask(
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
-console.log(tasks);
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
